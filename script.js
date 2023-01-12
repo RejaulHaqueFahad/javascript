d3b2448d27f6196a8b220c8fc587c823f4040f56
RejaulHaqueFahad <rejaulhaquefahad02@gmail.com>
2023-01-13 00:11:57 +0600

    javascripting

---
+++ script.js
@@ -0,0 +1,17 @@
+// let x = 5, y = "5"; z= x + y;
+// console.log(z);
+
+// document.getElementById("demo").innerHTML
+
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+
+// console.log(z)
+
+// for(let i = 1; i <= 100; i++) {
+//     document.write(i);
+// }
+
+
+
